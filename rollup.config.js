66d1caabf2878fcd5d377e64071c950dd700c93a
linux_china <libing.chen@gmail.com>
2020-06-24 16:37:34 -0700

    code format

--- rollup.config.js
+++ rollup.config.js
@@ -5,47 +5,47 @@ import html2 from "rollup-plugin-html2";
 import svelte from "rollup-plugin-svelte";
 import serve from "rollup-plugin-serve";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
+import {terser} from "rollup-plugin-terser";
 import livereload from "rollup-plugin-livereload";
 import sveltePreprocessor from "svelte-preprocess";
 
 const isDevelopment = process.env.NODE_ENV === "development";
 
 const plugins = [
-  svelte({
-    dev: isDevelopment,
-    extensions: [".svelte"],
-    preprocess: sveltePreprocessor(),
-    emitCss: true,
-  }),
-  postcss({
-    extract: true,
-  }),
-  typescript(),
-  commonjs({ include: "node_modules/**", extensions: [".js", ".ts"] }),
-  resolve(),
-  html2({
-    template: "src/index.html",
-  }),
+    svelte({
+        dev: isDevelopment,
+        extensions: [".svelte"],
+        preprocess: sveltePreprocessor(),
+        emitCss: true,
+    }),
+    postcss({
+        extract: true,
+    }),
+    typescript(),
+    commonjs({include: "node_modules/**", extensions: [".js", ".ts"]}),
+    resolve(),
+    html2({
+        template: "src/index.html",
+    }),
 ];
 if (isDevelopment) {
-  plugins.push(
-    serve({
-      contentBase: "./dist",
-      open: false,
-    }),
-    livereload({ watch: "./dist" })
-  );
+    plugins.push(
+        serve({
+            contentBase: "./dist",
+            open: false,
+        }),
+        livereload({watch: "./dist"})
+    );
 } else {
-  plugins.push(terser({ sourcemap: true }));
+    plugins.push(terser({sourcemap: true}));
 }
 
 module.exports = {
-  input: "src/index.ts",
-  output: {
-    file: "dist/index.js",
-    sourcemap: true,
-    format: "iife",
-  },
-  plugins,
+    input: "src/index.ts",
+    output: {
+        file: "dist/index.js",
+        sourcemap: true,
+        format: "iife",
+    },
+    plugins,
 };
